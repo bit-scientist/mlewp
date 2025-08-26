dd68bdd69819bd326d74ba35e02e5255e8123b83
26497571+bit-scientist <26497571+bit-scientist@users.noreply.github.com>
2025-08-26 11:47:20 +0900

    add insignificant cahnges

--- Chapter05/microservices/mlewp2-web-service/app/app.py
+++ Chapter05/microservices/mlewp2-web-service/app/app.py
@@ -8,6 +8,10 @@ import logging
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/')
+def health_check():
+    return "OK", 200
+
 forecaster = Forecaster()
 api.add_resource(ForecastHandler, '/forecast', resource_class_kwargs={'forecaster': forecaster})
 
@@ -16,3 +20,12 @@ if __name__ == '__main__':
     logging.info('Main app sequence begun')
     app.run(debug=True, host='0.0.0.0', port=5000) # change debug=False in production
     logging.info('App finished')
+
+'''
+{
+    "store_number": 10,
+    "forecast_start_date": "2023-07-01T00:00:00"
+}
+'''
+
+
